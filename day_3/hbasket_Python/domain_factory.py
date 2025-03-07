a91433838e60cfa854c036079c34f4072c08ff74
karthik-369 <85570839+karthik-369@users.noreply.github.com>
2025-03-07 15:46:12 +0530

    Delete day_3 directory

--- day_3/hbasket_Python/domain_factory.py
+++
@@ -1,9 +0,0 @@
-from h_basket import Item, HamaraBasket
-
-class DomainFactory:
-    def single_item_provider(self, name: str, sell_in: int, quality: int):
-        return [Item(name=name, sell_in=sell_in, quality=quality)]
-
-    def update_quality(self, items):
-        app = HamaraBasket(items)
-        app.update_quality()
